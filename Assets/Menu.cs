f7233751f4bad447fbfeb65961b0b0fdd4a3eb59
gfelipetto <gfelipetto@users.noreply.github.com>
2019-11-19 21:02:29 -0300

    Menu & Instruções

---
+++ Assets/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void iniciar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void instrucoes()
+    {
+        SceneManager.LoadScene("Instruções");
+    }
+
+    public void creditos()
+    {
+        SceneManager.LoadScene("Créditos");
+    }
+
+    public void voltarMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
