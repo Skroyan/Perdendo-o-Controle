2570702cc3dc95fe5ef5aa1bed37b0cf1ea223eb
Skroyan <tonipdorneles@gmail.com>
2019-11-20 22:40:43 -0300

    20.11
    Celular funcionando totalmente.
    Trigger para a mulher se movimentar ainda precisando de alguns ajustes, mas já encaminhado.

--- Assets/Celular.cs
+++ Assets/Celular.cs
@@ -1,36 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class Celular : MonoBehaviour
 {
 
-    private float contagem = 5;//Random.Range(8, 12);
+    private float contagem = 12;
     private bool cel = false;
-    private Vector3 mouse = Input.mousePosition;
+    RaycastHit hit;
+
+    [DllImport("user32.dll")]
+
+    static extern bool SetCursorPos(int X, int Y);
+    int xPos = 2000, yPos = 2020;
+    int sorteio;
+
+private void Awake()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Celular").transform.localScale = new Vector3(0, 0, 0); //Deixa invisível na tela
+        GameObject.Find("Celular").transform.localScale = new Vector3(0, 0, 0);//Deixa invisível na tela
     }
 
     void Update()
     {
         contagem -= Time.deltaTime;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-
-        if (contagem <= 0.0f)
+        if (contagem <= 0.0f && contagem >= -0.05f)
         { //Faz surgir o celular na tela
-            GameObject.Find("Celular").transform.localScale = new Vector3(0.14f, 0.14f, 0);
             cel = true;
+            GameObject.Find("Celular").transform.localScale = new Vector3(0.14f, 0.14f, 0);
+            sorteio = Random.Range(0, 4);
+
+            if (sorteio == 0)
+            {
+                xPos = 0;
+                yPos = 0;
+            }
+
+            else if (sorteio == 1)
+            {
+                xPos = 0;
+                yPos = 1080;
+            }
+
+            else if (sorteio == 2)
+            {
+                xPos = 1920;
+                yPos = 1080;
+            }
+
+            else if (sorteio == 3)
+            {
+                xPos = 1920;
+                yPos = 0;
+            }
+
+            SetCursorPos(xPos, yPos);
 
         }
 
-        if (cel = true && (mouse.x >=450 && mouse.x <= 916) && Input.GetButtonDown("Fire1")) {//Faz desaparecer o celular da tela
-            contagem = 50;
-            GameObject.Find("Celular").transform.localScale = new Vector3(0, 0, 0);;//Random.Range(8, 12);
-            cel = false;
+            if (cel = true && Input.GetButtonDown("Fire1")) {//Faz desaparecer o celular da tela
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.tag == "Celular")
+                {
+                    contagem = Random.Range(8f, 13f);
+                    GameObject.Find("Celular").transform.localScale = new Vector3(0, 0, 0);
+                    cel = false;
+                }
+            }
 
         }
     }
--- Assets/Personagens/StefTrigger.cs
+++ Assets/Personagens/StefTrigger.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class StefTrigger : MonoBehaviour
 {
-    public Transform[] posicoes = new Transform[2];
-    private RaycastHit gameObj;
+    
+    public GameObject obj;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject.Find("TriggerStefani").transform.localScale = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*private void OnTriggerStay()
+        {
+            float velocidade = 15f;
+            GameObject.Find("Stefani").transform.Translate(+((velocidade) * Time.deltaTime), 0, 0);
+        }
+
+        private void OnTriggerExit()
+        {
+            texto.text = "";
+        }
         bool chegou = Physics.Linecast(posicoes[0].position, posicoes[1].position, out gameObj);
 
         if (chegou)
@@ -25,6 +35,12 @@ public class StefTrigger : MonoBehaviour
                 float velocidade = 15f;
                 GameObject.Find("Stefani").transform.Translate(+((velocidade) * Time.deltaTime), 0, 0);
             }
-        }
+        }*/
+    }
+
+    private void OnTriggerStay()
+    {
+        float velocidade = 20f;
+        GameObject.Find("Stefani").transform.Translate(+((velocidade) * Time.deltaTime), 0, 0);
     }
 }
