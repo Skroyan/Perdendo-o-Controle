2a30a3920523b29710f4b9776e8fa83dac9aff8a
Skroyan <tonipdorneles@gmail.com>
2019-11-25 09:29:06 -0300

    25.11
    Mais umas coisinhas.

---
+++ Assets/PassarFase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassarFase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("colidiu");
+        if (other.gameObject.tag == "Tocus")
+        {
+            SceneManager.LoadScene("Fase2");
+        }
+    }
+}
--- Assets/Sons/Colisões.cs
+++ Assets/Sons/Colisões.cs
@@ -7,11 +7,16 @@ public class Colisões : MonoBehaviour
 {
 
     public AudioSource colisao;
+    public AudioSource mulher;
+    public AudioSource cachorro;
+
     //public AudioClip som;
 
     void Start()
     {
         colisao = GameObject.Find("Tocus").GetComponent<AudioSource>();
+        mulher = GameObject.Find("GritoMulher").GetComponent<AudioSource>();
+        cachorro = GameObject.Find("GritoCachorro").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,8 +31,13 @@ public class Colisões : MonoBehaviour
         if (other.gameObject.tag == "Tocus")
         {
             //GameObject.Find("Tocus").GetComponent<AudioSource>().PlayOneShot(som);
-            colisao.Play();
-            //SceneManager.LoadScene("Fase2");
+
+            if (!colisao.isPlaying)
+            {
+                colisao.Play();
+            }
+
+            SceneManager.LoadScene("Notícias");
         }
     }
 
