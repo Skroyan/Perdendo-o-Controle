0ec740943d2f24cec4530be66e44fde10f07e5c1
Skroyan <tonipdorneles@gmail.com>
2019-11-25 08:27:27 -0300

    25.11
    Colisores de prédios e postes colocados, UM POR UM, MANUALMENTE SAPORRA '-'

--- Assets/Personagens/StefTrigger.cs
+++ Assets/Personagens/StefTrigger.cs
@@ -10,7 +10,7 @@ public class StefTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("TriggerStefani").transform.localScale = new Vector3(0, 0, 0);
+        //this.transform.localScale = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
---
+++ Assets/Sons/ColisorPos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisorPos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.localPosition = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (transform.parent.position != transform.position)
+        //{
+        //    transform.parent.position = transform.position;
+        //}
+        //transform.localPosition = Vector3.zero;
+    }
+
+    void Example()
+    {
+        // Get the y component of the position relative to the parent
+        // and print it to the Console
+        //print(transform.localPosition.y);
+    }
+}
