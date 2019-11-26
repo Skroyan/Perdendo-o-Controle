767887e0974099126f67de5060cad8b581e55974
Skroyan <54299210+Skroyan@users.noreply.github.com>
2019-11-26 18:11:06 -0200

    26.11
    Capotar funcionando + colisores da fase 1

---
+++ Assets/Capotar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Capotar : MonoBehaviour
+{
+    Transform rotacao;
+    float contagem = 0.7f;
+    bool capotou = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotacao = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.eulerAngles.z >= 90f && this.transform.eulerAngles.z <= 300f)
+        {
+            capotou = true;
+        }
+
+        if (capotou == true)
+        {
+            contagem -= Time.deltaTime;
+        }
+
+        if (contagem <= 0f)
+        {
+            SceneManager.LoadScene("Notícias");
+        }
+
+
+    }
+
+}
